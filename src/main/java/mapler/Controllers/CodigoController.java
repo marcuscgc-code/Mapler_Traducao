6ab79cbd689b625f396da342c95dd751bec7bf46
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-08 22:16:01 -0300

    config-inicial

---
+++ src/main/java/mapler/Controllers/CodigoController.java
@@ -0,0 +1,13 @@
+package mapler.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CodigoController {
+
+    @GetMapping("/")
+    public String index() {
+       return "index";
+    }
+}
---
+++ src/test/java/ifma/edu/mapler/compiler/CompiladorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ifma.edu.mapler.compiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompiladorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
