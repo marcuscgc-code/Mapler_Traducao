3494fe5ad7cb89b2a3668ade4c8d92b0220f4590
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-22 15:43:51 -0300

    exemplos-refector

--- src/main/java/mapler/Controllers/ExemploController.java
+++ src/main/java/mapler/Controllers/ExemploController.java
@@ -8,6 +8,30 @@ public class ExemploController {
 
     @GetMapping("/exemplo")
     public String exemplo() {
-        return "exemplo";
+        return "exemplos/estrutura-codigo";
     }
+
+    @GetMapping("/exemplo/estrutura-codigo")
+    public String exemploEstrutura() {
+        return "exemplos/estrutura-codigo";
+    }
+
+    @GetMapping("/exemplo/entrada-saida")
+    public String exemploEntrada(){ return  "exemplos/entrada-saida"; }
+
+    @GetMapping("/exemplo/modulos")
+    public String exemploModulos() { return "exemplos/modulos"; }
+
+    @GetMapping("/exemplo/tipos")
+    public String exemploTipos() { return "exemplos/tipos"; }
+
+    @GetMapping("/exemplo/operacoes")
+    public String exemploOperacoes() { return "exemplos/operacoes"; }
+
+    @GetMapping("/exemplo/condicionais")
+    public String exemploCondicionais() { return "exemplos/condicionais"; }
+
+    @GetMapping("/exemplo/lacos")
+    public String exemploLacos() { return "exemplos/lacos"; }
+
 }
