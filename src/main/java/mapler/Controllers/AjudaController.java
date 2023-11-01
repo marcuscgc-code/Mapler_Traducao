f8670fdf98a996cde2d8a89d04abec72773cb831
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-11-01 15:22:42 -0300

    Create AjudaController.java

---
+++ src/main/java/mapler/Controllers/AjudaController.java
@@ -0,0 +1,11 @@
+package mapler.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AjudaController {
+    @GetMapping("/ajuda")
+    public String ajuda() {
+        return "ajuda";
+    }
+}
